48c143b282a11400e3f65a42df042bb8c306c8e7
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-11-18 00:04:16 +0900

    Add accept_connection and recieve_file function

--- server.c
+++ server.c
@@ -40,8 +40,46 @@ int do_listen(int sock){
 	return TRUE;
 }
 
+int accept_connection(int sock){
+	int new_sock;
+	int addr_len;
+	struct sockaddr_in writer_sin;
+
+	memset(&writer_sin, 0, sizeof(struct sockaddr_in));
+	writer_sin.sin_family = AF_INET;
+	writer_sin.sin_port = htons(PORT_USED);
+	writer_sin.sin_addr.s_addr = htons(INADDR_ANY);
+
+	addr_len = sizeof(writer_sin);
+
+	while(1){
+		if((new_sock=accept(sock, (struct sockaddr*)&writer_sin, (socklen_t*)&addr_len))<0){
+			perror("Failed to accept\n");
+			return FALSE;
+		}
+		return new_sock;
+	}
+}
+
+int recieve_file(int sock, FILE* file, long* file_size){
+
+	if((*file_size = recv(sock, (void*)file, sizeof(FILE), 0))<0){
+		perror("Failed to recv\n");
+		close(sock);
+		return FALSE;
+	}
+	printf("File recieved\nFile size: %dbyte\n", *file_size);
+
+	return TRUE;
+}
+
+
 int main(){
-	int sock;
+	int sock, new_sock;
+	long file_size;
+	FILE file;
+
+	memset(&file, 0, sizeof(FILE));
 
 	if((sock = open_and_bind_socket())<0){
 		perror("Failed to open_and_bind_socket()\n");
@@ -54,6 +92,20 @@ int main(){
 		return FALSE;
 	}
 
+	if((new_sock = accept_connection(sock))<0){
+		perror("Failed to accept_connection()\n");
+		close(sock);
+		return FALSE;
+	}
+
+	if(recieve_file(new_sock, &file, &file_size)<0){
+		perror("Failed to recieve_file()\n");
+		close(new_sock);
+		return FALSE;
+	}
+
+	
 	close(sock);
+	close(new_sock);
 	return TRUE;
 }
